dd4ae7ebf79ed0f92f90350f32e2776fb4429112
mateotis <hekfuszmate@gmail.com>
2019-11-05 14:43:51 +0400

    Made tokeniser work with (hopefully) all edge cases.

--- tokeniser.cpp
+++ tokeniser.cpp
@@ -19,7 +19,7 @@ vector<string> tokenise(string infix) {
 		while((infix.at(i) == '(') || (infix.at(i) == ')')) {
 			string paren(1, infix.at(i)); // Since parantheses can only be one character each, we can safely convert them to string
 			exp.push_back(paren);
-			//cout << paren << " is parantheses." << endl;
+			cout << paren << " is parantheses." << endl;
 			if(i < infix.length() - 1) { // Boundary check
 				i++;
 			}
@@ -38,16 +38,22 @@ vector<string> tokenise(string infix) {
 			}
 		}
 		if(operand != "") { // To avoid pushing empty values into the vector
-			//cout << operand << " is a number." << endl;
+			cout << operand << " is a number." << endl;
 			exp.push_back(operand);
 		}
 		operand = "";
 
 		while(isdigit(infix.at(i)) == false && infix.at(i) != '(' && infix.at(i) != ')') { // Covers multi-character operators
-			if(infix.at(i) == '-' && isdigit(infix.at(i-1)) == false && isdigit(infix.at(i+1)) == true) {  // This handles - as a negative marker which should be part of an operand
+			if(infix.at(i) == '-' && infix.at(i+1) == '-') { // Edge case for --
+				cout << "-- is an operator." << endl;
+				exp.push_back("--");
+				i += 2;
+			}
+			else if(infix.at(i) == '-' && i == 0 && isdigit(infix.at(i+1)) == true) { // Edge case where there is a negative at the start of the expression
 				operand += infix.at(i);
 				i++;
 				while(isdigit(infix.at(i)) == true) {
+					//cout << "in here" << endl;
 					operand += infix.at(i); // Operands and operators are tokenised into strings
 					if(i < infix.length() - 1) { // Boundary check
 						i++;
@@ -57,8 +63,29 @@ vector<string> tokenise(string infix) {
 					}					
 				}
 				if(operand != "") {
-					exp.push_back(op);
-					operand = "";				
+					cout << operand << " is a number." << endl;
+					exp.push_back(operand);
+					operand = "";
+				}
+
+			}
+			else if(infix.at(i) == '-' && isdigit(infix.at(i+1)) == true && isdigit(infix.at(i-1)) == false) {  // This handles - as a negative marker which should be part of an operand
+				operand += infix.at(i);
+				i++;
+				while(isdigit(infix.at(i)) == true) {
+					//cout << "in here" << endl;
+					operand += infix.at(i); // Operands and operators are tokenised into strings
+					if(i < infix.length() - 1) { // Boundary check
+						i++;
+					}
+					else {
+						break;
+					}					
+				}
+				if(operand != "") {
+					cout << operand << " is a number." << endl;
+					exp.push_back(operand);
+					operand = "";
 				}
 
 			}
@@ -74,7 +101,7 @@ vector<string> tokenise(string infix) {
 		}
 		
 		if(op != "") {
-			//cout << op << " is an operator." << endl;
+			cout << op << " is an operator." << endl;
 			exp.push_back(op);			
 		}
 		op = "";
@@ -85,7 +112,7 @@ vector<string> tokenise(string infix) {
 
 int main() {
 
-	string infix = "120//(343+(546-781*5))";
+	string infix = "--120//((--343)+(546-781*(**5)))";
 	vector<string> expression = tokenise(infix);
 
 	for(int i = 0; i < expression.size(); i++) {
