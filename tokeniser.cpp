ccfacc1e96a89efe676cf78017d2aa17c9134606
mateotis <hekfuszmate@gmail.com>
2019-11-07 11:20:50 +0400

    Completely rewrote the tokeniser: now it's more intuitive and robust.

--- tokeniser.cpp
+++ tokeniser.cpp
@@ -14,98 +14,126 @@ vector<string> tokenise(string infix) {
 
 	string operand = "";
 	string op = ""; // "operator" is not a valid variable name, sadly
+	string var = "";
 
 	int i = 0;
-	while(i < infix.length() - 1) {
-		while((infix.at(i) == '(') || (infix.at(i) == ')')) {
-			string paren(1, infix.at(i)); // Since parantheses can only be one character each, we can safely convert them to string
-			exp.push_back(paren);
-			cout << paren << " is parantheses." << endl;
-			if(i < infix.length() - 1) { // Boundary check
-				i++;
+
+	while(i < infix.length()) {
+		char elem = infix.at(i);
+
+		if(infix.length() == 1) { // If the whole expression is one character, just add it and be done with it
+			string elemS(1, elem);
+			exp.push_back(elemS);
+			return exp;
+		}
+
+		if(i == infix.length() - 1) { // If we're on the last character, then we add it to the last unfinished token before returning
+			if(op != "") {
+				op += elem;
+				exp.push_back(op);
+				return exp; 
 			}
-			else {
-				break;
+			else if(operand != "") {
+				operand += elem;
+				exp.push_back(operand); 
+				return exp; 
+			}
+			else if(var != "") {
+				var += elem;
+				exp.push_back(operand); 
+				return exp; 
+			}
+			else { // If it's none of these, it's a parentheses
+				string elemS(1, elem);
+				exp.push_back(elemS);
+				return exp;
 			}
 		}
 
-		while(isdigit(infix.at(i)) == true) { // All the digits that are next to each other make one number
-			operand += infix.at(i); // Operands and operators are tokenised into strings
-			if(i < infix.length() - 1) { // Boundary check
-				i++;
-			}
-			else {
-				break;
-			}
-		}
-		if(operand != "") { // To avoid pushing empty values into the vector
-			cout << operand << " is a number." << endl;
-			exp.push_back(operand);
-		}
-		operand = "";
-
-		while(isdigit(infix.at(i)) == false && infix.at(i) != '(' && infix.at(i) != ')') { // Covers multi-character operators
-			if(infix.at(i) == '-' && infix.at(i+1) == '-') { // Edge case for --
-				cout << "-- is an operator." << endl;
-				exp.push_back("--");
-				i += 2;
-			}
-			else if(infix.at(i) == '-' && i == 0 && isdigit(infix.at(i+1)) == true) { // Edge case where there is a negative at the start of the expression
-				operand += infix.at(i);
-				i++;
-				while(isdigit(infix.at(i)) == true) {
-					//cout << "in here" << endl;
-					operand += infix.at(i); // Operands and operators are tokenised into strings
-					if(i < infix.length() - 1) { // Boundary check
-						i++;
-					}
-					else {
-						break;
-					}					
-				}
-				if(operand != "") {
-					cout << operand << " is a number." << endl;
-					exp.push_back(operand);
-					operand = "";
-				}
-
-			}
-			else if(infix.at(i) == '-' && isdigit(infix.at(i+1)) == true && isdigit(infix.at(i-1)) == false) {  // This handles - as a negative marker which should be part of an operand
-				operand += infix.at(i);
-				i++;
-				while(isdigit(infix.at(i)) == true) {
-					//cout << "in here" << endl;
-					operand += infix.at(i); // Operands and operators are tokenised into strings
-					if(i < infix.length() - 1) { // Boundary check
-						i++;
-					}
-					else {
-						break;
-					}					
-				}
-				if(operand != "") {
-					cout << operand << " is a number." << endl;
-					exp.push_back(operand);
-					operand = "";
-				}
-
-			}
-			else {
-				op += infix.at(i);
-				if(i < infix.length() - 1) {
-					i++;
-				}
-				else {
-					break;
-				}
-			}
+		// Otherwise, we head into tokenisation...
+		if(isspace(elem)) { // Filtering out whitespace
+			cout << i << " is a space." << endl;
+			if(i < infix.length() - 1) { i++; continue; }
+			else { break; }			
 		}
 		
-		if(op != "") {
-			cout << op << " is an operator." << endl;
-			exp.push_back(op);			
+		else if(elem == '(' || elem == ')') { // Parentheses
+			cout << i << " is a parentheses." << endl;
+			string paren(1, elem);
+			exp.push_back(paren);
+			if(i < infix.length() - 1) { i++; continue; }
+			else { break; }
 		}
-		op = "";
+
+		else if(isalpha(elem)) { // Variables
+			cout << i << " is a variable." << endl;
+			var += elem;
+			if(isalpha(infix.at(i+1)) == 0) {
+				exp.push_back(var);
+				var = "";
+			}
+			if(i < infix.length() - 1) { i++; continue; }
+			else { break; }
+		}
+
+		else if(isdigit(elem)) { // Numbers
+			cout << i << " is a number." << endl;
+			operand += elem;
+			if(isdigit(infix.at(i+1)) == 0) {
+				exp.push_back(operand);
+				operand = "";
+			}
+			if(i < infix.length() - 1) { i++; continue; }
+			else { break; }			
+		}
+
+		else { // Operators of all kinds
+			if(elem == '-' && infix.at(i+1) == '-') { // Edge case for --
+				cout << i << " is a -- operator." << endl;
+				exp.push_back("--");
+				if(i < infix.length() - 2) { i = i + 2; continue; }
+				else { break; }				
+			}
+			else if(i == 0 && elem == '-' && (isdigit(infix.at(i+1)) || isalpha(infix.at(i+1))) ) { // Edge case for a negative number at the start (avoids the i-1 checks)
+				if(isdigit(infix.at(i+1))) {
+					operand += elem;
+					cout << i << " is a negative number." << endl;
+					if(i < infix.length() - 1) { i++; continue; }
+					else { break; }
+				}
+				else {
+					var += elem;
+					cout << i << " is a negative variable." << endl;
+					if(i < infix.length() - 1) { i++; continue; }
+					else { break; }
+				}				
+			}
+			else if(i != 0 && elem == '-' && isdigit(infix.at(i-1)) == 0 && isalpha(infix.at(i-1)) == 0 && (isdigit(infix.at(i+1)) || isalpha(infix.at(i+1))) ) { // When - is a negative marker instead of an operator
+				if(isdigit(infix.at(i+1))) {
+					operand += elem;
+					cout << i << " is a negative number." << endl;
+					if(i < infix.length() - 1) { i++; continue; }
+					else { break; }
+				}
+				else {
+					var += elem;
+					cout << i << " is a negative variable." << endl;
+					if(i < infix.length() - 1) { i++; continue; }
+					else { break; }
+				}
+			}
+			else { // All other operators
+				op += elem;
+				cout << i << " is an operator." << endl;
+				if(isalpha(infix.at(i+1)) || isdigit(infix.at(i+1)) || isspace(infix.at(i+1)) || infix.at(i+1) == '(' || infix.at(i+1) == ')') {
+					exp.push_back(op);
+					op = "";
+				}
+				if(i < infix.length() - 1) { i++; continue; }
+				else { break; }
+			}
+		}
+
 	}
 
 	return exp;
@@ -122,7 +150,7 @@ struct Expression {
 int main() {
 
 	vector<Expression> expressions;
-	ifstream fin("file3.txt");
+	ifstream fin("file1.txt");
 
 	if (fin.is_open()) {
 		string line;
@@ -136,7 +164,7 @@ int main() {
 			int valueEnd = line.find(";");
 
 			if(valueEnd != string::npos && line.at(line.length() - 2) == ' ' && line.at(nameEnd-1) == ' ' && line.at(nameEnd+1) == ' ') { // Sanity checks to ensure proper semicolon and equal sign placement
-
+				// TODO: Add check for equal number of left and right parantheses
 				//cout << nameEnd << endl;
 				for(int i = 0; i < nameEnd - 1; i++) { // Parses variable names
 					expName += line.at(i);
@@ -158,7 +186,7 @@ int main() {
 				}
 			}
 			else {
-				cout << "Invalid syntax on line " << lineCount << "." << endl;
+				cout << "Invalid syntax on line " << lineCount << ". Expression will not be evaluated." << endl;
 			}
 
 
@@ -167,14 +195,27 @@ int main() {
 	fin.close();
 
 	for(int i = 0; i < expressions.size(); i++) {
-		cout << expressions[i].name << " = " << expressions[i].exp << endl;
+		cout << expressions[i].exp << endl;
 	}
 
-	string infix = "--120//((--343)+(546-781*(**5)))";
-/*	vector<string> expression = tokenise(infix);
+	//string infix = "1+2+3+4";
+	//vector<string> expression = tokenise(infix);
 
-	for(int i = 0; i < expression.size(); i++) {
+
+
+ 	for(int i = 0; i < expressions.size(); i++) {
+ 		Expression expToTokenise = expressions.at(i);
+ 		vector<string> tokenisedExp = tokenise(expToTokenise.exp);
+
+ 		cout << "EXPRESSION IS: ";
+	 	for(int i = 0; i < tokenisedExp.size(); i++) {
+			cout << tokenisedExp.at(i) << ',';
+		}
+		cout << endl;
+ 	}
+
+/*	for(int i = 0; i < expression.size(); i++) {
 		cout << expression.at(i);
-	}
-	cout << endl;*/
+	}*/
+	cout << endl;
 }
