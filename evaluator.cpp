586e9a509a010985ad087e5e6b15b47bb9ed5d1d
Li Cheng <licheng@Lis-MacBook-Pro-3.local>
2019-11-10 20:27:07 +0400

    Added unary and binary check.

--- evaluator.cpp
+++ evaluator.cpp
@@ -1,15 +1,12 @@
 // Evaluator: evaluates the preprocessed postfix expressions and returns their results
-
 #include<iostream>
 #include<string>
 #include<cmath>
 #include<stack>
 #include<ctype.h>
 #include<vector>
-
 #include "preprocessor.h"
 #include "evaluator.h"
-
 using namespace std;
 
 float evaluate(vector<string> postfix)	//Method which will evaluate a PostfixExpression and return the result
@@ -19,7 +16,7 @@ float evaluate(vector<string> postfix)	//Method which will evaluate a PostfixExp
 	for(int i = 0; i < postfix.size(); i++) {
 		//	a.	if the element is an operand push it to the stack
 
-		if(isOperator(postfix.at(i)) == false) {
+/*		if(isOperator(postfix.at(i)) == false) {
 
 			//convert string into float element
 
@@ -42,9 +39,82 @@ float evaluate(vector<string> postfix)	//Method which will evaluate a PostfixExp
 
 			myStack.push(result);
 		}
-
-
 	}
 	//3. return the value from the top of the stack (i.e. the final answer)
 	return myStack.top();
 }
+*/
+		if(isOperator(postfix.at(i)) == false) {
+			//convert string into float element
+			float elem = stof(postfix.at(i));
+			myStack.push(elem);
+		}
+		//	b.	if the element is an operator pop 2 elements from the stack, apply the operator on it and push the result back to the stack
+		
+		if (postfix.at(i) =="-")
+		{
+
+			if (myStack.size()==2) //if it is a binary minus
+			{
+				float a = myStack.top();
+				myStack.pop();
+				float b = myStack.top();
+				myStack.pop();
+				float result;
+				result = b - a;
+				myStack.push(result);
+			}
+				
+			else if (myStack.size()==1)   //if it is a unary minus
+			{
+				float a = myStack.top();
+				myStack.pop();
+				float result;
+				result=-a;
+				myStack.push(result);
+			}
+		}
+
+		else if(isOperator(postfix.at(i)) and isUnary(postfix.at(i))) {
+			float a = myStack.top();
+			myStack.pop();
+			float b = myStack.top();
+			myStack.pop();
+			float result;
+			//cout << postfix.at(i) << endl;
+			if (postfix.at(i)=="+"){result = b + a; }
+			//else if (postfix.at(i)=="-"){result = b - a; }
+			else if (postfix.at(i)=="*"){result = b * a; }
+			else if (postfix.at(i)=="/"){result = b / a; }
+			else if (postfix.at(i)=="%"){result= fmodf(b, a);}
+			else if (postfix.at(i)=="//"){result = (b - fmodf(b, a))/a; } 
+			myStack.push(result);
+		}
+			
+		//if it is a unary operator
+		else if (isOperator(postfix.at(i)) and !isUnary(postfix.at(i)))
+		{
+			float a = myStack.top();
+			myStack.pop();
+			float result;
+			if (postfix.at(i)=="**"){result = a * a; }
+			else if (postfix.at(i)=="--"){result = a-1; }
+			else if (postfix.at(i)=="++"){result = a+1; }
+			myStack.push(result);
+		}
+	}
+	//3. return the value from the top of the stack (i.e. the final answer)
+	return myStack.top();
+}
+
+bool isUnary(string op)
+{
+	if (op=="+" || op=="*" || op=="/" || op=="^" || op=="%" || op=="//")
+		return true;
+	else if (op=="--"|| op== "**" || op== "++")
+		return false;
+
+
+}
+
+//check if stack has two or one element, if it has two, then binary, if one, then unary
